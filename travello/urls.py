5ccd947bf9e4d6948892ea5258ed8a1c20b65636
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2022-11-11 08:07:33 +0200

    Add funds Form

--- travello/urls.py
+++ travello/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('destination_list/destination_details/<str:city_name>', views.destination_details, name='destination_details'),
     path('destination_details/<str:city_name>', views.destination_details, name='destination_details'),
     path('destination_list/destination_details/pessanger_detail_def/<str:city_name>',views.pessanger_detail_def,name='pessanger_detail_def'),
+    path('add_funds',views.add_funds,name='add_funds'),
     path('upcoming_trips', views.upcoming_trips, name='upcoming_trips'),
     path('destination_list/destination_details/pessanger_detail_def/pessanger_detail_def/card_payment', views.card_payment, name='card_payment'),
     path('destination_list/destination_details/pessanger_detail_def/pessanger_detail_def/otp_verification', views.otp_verification, name='otp_verification'),
--- travello/views.py
+++ travello/views.py
@@ -243,6 +243,52 @@ def pessanger_detail_def(request, city_name):
 
         return render(request, 'sample.html', {'formset': formset, 'city_name': city_name, })
 
+def add_funds(request):
+    KeyValueFormSet = formset_factory(KeyValueForm, extra=1)
+    if request.method == 'POST':
+        formset = KeyValueFormSet(request.POST)
+        if formset.is_valid():
+            temp_date = datetime.strptime(request.POST['trip_date'], "%Y-%m-%d").date()
+            date1 = datetime.now().date()
+            if temp_date < date1:
+                return redirect('index')
+            obj = pessanger_detail.objects.get(Trip_id=3)
+            pipo_id = obj.Trip_same_id
+            #pipo_id =4
+            request.session['Trip_same_id'] = pipo_id
+            price = request.session['price']
+            city = request.session['city']
+            print(request.POST['trip_date'])
+            #temp_date = parse_date(request.POST['trip_date'])
+            temp_date = datetime.strptime(request.POST['trip_date'], "%Y-%m-%d").date()
+            usernameget = request.user.get_username()
+            print(temp_date)
+            request.session['n']=formset.total_form_count()
+            for i in range(0, formset.total_form_count()):
+                form = formset.forms[i]
+
+                t = pessanger_detail(Trip_same_id=pipo_id,first_name=form.cleaned_data['first_name'], last_name=form.cleaned_data['last_name'],
+                                     age=form.cleaned_data['age'],
+                                     Trip_date=temp_date,payment=price,username=usernameget,city=city)
+                t.save()
+                # print (formset.forms[i].form-[i]-value)
+
+            obj.Trip_same_id = (pipo_id + 1)
+            obj.save()
+            no_of_person = formset.total_form_count()
+            price1 = no_of_person * price
+            GST = price1 * 0.18
+            GST = float("{:.2f}".format(GST))
+            final_total = GST + price1
+            request.session['pay_amount'] = final_total
+            return render(request,'payment.html', {'no_of_person': no_of_person,
+                                                   'price1': price1, 'GST': GST, 'final_total': final_total,'city': city })
+    
+    else:
+        formset = KeyValueFormSet()
+
+        return render(request, 'paynow.html', {'formset': formset })
+
 
 def upcoming_trips(request):
     username = request.user.get_username()
